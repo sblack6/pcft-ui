91e378128c8be0595cf5ac3363c3f322019befae
Sean Black <black.sean14@gmail.com>
2022-04-22 14:35:21 -0700

    Normalize date-range-picker output to API format

--- src/app/date-picker/range/date-range-picker.component.ts
+++ src/app/date-picker/range/date-range-picker.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { convertDatePickerOutputToApiDate } from '../date-picker-utility';
 
 @Component({
   selector: 'app-date-range-picker',
@@ -16,6 +17,10 @@ export class DateRangePickerComponent {
   });
 
   onDateChange() {
+    this.range.setValue({
+      start: convertDatePickerOutputToApiDate(JSON.stringify(this.range.value.start)),
+      end: convertDatePickerOutputToApiDate(JSON.stringify(this.range.value.end))
+    });
     this.dateChanged.emit(this.range.value);
   }
 
