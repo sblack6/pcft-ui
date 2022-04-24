f1367c65cecd2964269af4d2fdd83fc41d47ffc5
Sean Black <black.sean14@gmail.com>
2022-04-24 09:58:33 -0700

    Add detail-table to budget home and add route to budget home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BudgetEditorComponent } from './budget/budget-editor/budget-editor.component';
+import { BudgetHomeComponent } from './budget/budget-home/budget-home.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   // Add new paths here (order matters)
+  { path: 'budget', component: BudgetHomeComponent },
   { path: 'edit-budget', component: BudgetEditorComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', redirectTo: 'home', pathMatch: 'full' }
