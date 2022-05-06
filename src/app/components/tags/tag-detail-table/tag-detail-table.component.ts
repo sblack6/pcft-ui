797d532569f2cfe7a276e1d8685bc583d3cc5f00
Sean Black <black.sean14@gmail.com>
2022-05-06 12:23:05 -0700

    Tag detail table barebones impl

--- src/app/components/tags/tag-detail-table/tag-detail-table.component.ts
+++ src/app/components/tags/tag-detail-table/tag-detail-table.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Transaction } from 'src/app/model/transaction';
+import { findAllCategories, findAllTags } from 'src/app/shared/transaction-utility-functions';
 
 @Component({
   selector: 'app-tag-detail-table',
@@ -34,12 +35,62 @@ export class TagDetailTableComponent {
     return this._tagSelected;
   }
 
+  total: number;
+
+  categoryTotals: {category: string, total: number}[];
+
+  tagTotals: {tag: string, total: number}[];
+
   populateTableData() {
     if (!this.tagSelected || !this.transactions) {
       return;
     }
+    const transactionsForTag = this.transactions.filter(transaction => transaction.tags && transaction.tags.includes(this.tagSelected));
+    console.log('Transactions for tag: ', transactionsForTag)
+    this.total = transactionsForTag.map(transaction => transaction.amount).reduce((partialSum, a) => partialSum + a, 0);
+    this.findCategoryTotals(transactionsForTag);
+    this.findTagTotals(transactionsForTag);
+  }
 
-    
+  findCategoryTotals(transactions: Transaction[]) {
+    const categories = findAllCategories(transactions);
+    const categoryTotals = [];
+    categories.forEach(category => {
+      const amount = transactions.filter(transaction => transaction.category === category)
+        .map(transcation => transcation.amount)
+        .reduce((partialSum, a) => partialSum + a, 0);
+      categoryTotals.push({
+        category: category,
+        total: amount,
+      });
+    });
+    this.categoryTotals = categoryTotals;
+  }
+
+  findTagTotals(transactions: Transaction[]) {
+    const tags = findAllTags(transactions);
+    tags.delete(this.tagSelected);
+    const tagTotals = [];
+    // Find the total of transactions containing ONLY the selected tag.
+    const amount = transactions.filter(transaction => transaction.tags === this.tagSelected)
+        .map(transcation => transcation.amount)
+        .reduce((partialSum, a) => partialSum + a, 0);
+    tagTotals.push({
+      tag: this.tagSelected,
+      total: amount,
+    });
+    // Find the total of transactions for all other tags.
+    tags.forEach(tag => {
+      const amount = transactions.filter(transaction => transaction.tags.includes(tag))
+        .map(transcation => transcation.amount)
+        .reduce((partialSum, a) => partialSum + a, 0);
+      tagTotals.push({
+        tag: tag,
+        total: amount,
+      });
+    });
+
+    this.tagTotals = tagTotals;
   }
 
 }
