3d2463535e48f45b75c21222b63f466b9e62acee
Sean Black <black.sean14@gmail.com>
2022-05-06 11:14:30 -0700

    Refactor category-tag-detail: make it a dumb component where all data is input

--- src/app/components/tags/category-tag-detail/category-tag-detail.component.ts
+++ src/app/components/tags/category-tag-detail/category-tag-detail.component.ts
@@ -1,10 +1,9 @@
 import { Component, Input } from '@angular/core';
 import * as Highcharts from 'highcharts';
 import { Transaction } from 'src/app/model/transaction';
-import { TransactionService } from 'src/app/service/transaction/transaction.service';
 import { getMonth, getMonthRange, getMonthYear, getYear } from 'src/app/shared/date-utility-functions';
 import { monthNameMap, TYPE_BUDGET, TYPE_TRANSACTION } from 'src/app/shared/transaction-constants';
-import { findAllCategories, findAllTags, includesTag } from 'src/app/shared/transaction-utility-functions';
+import { findAllTags, includesTag } from 'src/app/shared/transaction-utility-functions';
 import { DateRange } from '../../date-picker/range/date-range-picker.component';
 
 @Component({
@@ -13,6 +12,35 @@ import { DateRange } from '../../date-picker/range/date-range-picker.component';
   styleUrls: ['./category-tag-detail.component.css']
 })
 export class CategoryTagDetailComponent {
+
+  @Input() dateRange: DateRange;
+
+  _transactions: Transaction[];
+
+  @Input() set transactions(value: Transaction[]) {
+    if (value) {
+      this._transactions = value;
+      this.setDateRange()
+      this.populateChartData();
+    }
+  }
+
+  get transactions(): Transaction[] {
+    return this._transactions;
+  }
+
+  _categorySelected: string;
+
+  @Input() set categorySelected(value: string) {
+    if (value) {
+      this._categorySelected = value;
+      this.populateChartData();
+    }
+  }
+
+  get categorySelected(): string {
+    return this._categorySelected;
+  }
 
   highcharts = Highcharts;
 
@@ -40,44 +68,12 @@ export class CategoryTagDetailComponent {
     series: [],
   };
 
-  allTransactions: Transaction[];
-
   monthRange: string[];
 
   isLoading = true;
 
-  _dateRange: DateRange;
-  @Input() set dateRange(value: DateRange) {
-    if (value) {
-      this._dateRange = value;
-      this.setDateRange();
-      this.loadTransactions();
-    }
-  }
-
-  get dateRange(): DateRange {
-    return this._dateRange;
-  }
-
-  categoryOptions: {label: string, value: string}[];
-
-  _categorySelected: string;
-
-  set categorySelected(value: string) {
-    if (value) {
-      this._categorySelected = value;
-      this.populateChartData();
-    }
-  }
-
-  get categorySelected(): string {
-    return this._categorySelected;
-  }
-
-  constructor(private transactionService: TransactionService) {}
-
   setDateRange() {
-    this.monthRange = getMonthRange(this._dateRange);
+    this.monthRange = getMonthRange(this.dateRange);
     this.defaultOptions = {
       ...this.defaultOptions,
       xAxis: {
@@ -91,32 +87,17 @@ export class CategoryTagDetailComponent {
     }
   }
 
-  loadTransactions() {
-    this.transactionService.search(this.dateRange.start, this.dateRange.end).subscribe((data: Transaction[]) => {
-      this.allTransactions = data;
-      this.populateCategoryOptions();
-      this.isLoading = false;
-    });
-  }
-
-  populateCategoryOptions() {
-    const categories = findAllCategories(this.allTransactions);
-    this.categoryOptions = Array.from(categories).map(category => {
-      return {
-        label: category.substring(0,1).toUpperCase() + category.substring(1),
-        value: category
-      }
-    });
-    this.categorySelected = this.categoryOptions ? this.categoryOptions[0].value : '';
-  }
-
   populateChartData() {
-    const transactionsForCategory = this.allTransactions.filter(transaction => transaction.category === this.categorySelected);
+    if (!this.transactions || !this.categorySelected) {
+      return;
+    }
+    const transactionsForCategory = this.transactions.filter(transaction => transaction.category === this.categorySelected);
     const series: any[] = [];
     this.populateTagTimeSeriesData(transactionsForCategory, series);
     this.populateBudgetTimeSeriesData(transactionsForCategory, series);
     this.series = series;
     this.updateChartData();
+    this.isLoading = false;
   }
 
   populateTagTimeSeriesData(transactionsForCategory: Transaction[], series: any[]) {
--- src/app/components/tags/tag-home/tag-home.component.ts
+++ src/app/components/tags/tag-home/tag-home.component.ts
@@ -1,17 +1,42 @@
-import { Component } from '@angular/core';
-import { DEFAULT_DATE_RANGE } from 'src/app/shared/transaction-constants';
+import { Component, OnInit } from '@angular/core';
+import { Transaction } from 'src/app/model/transaction';
+import { TransactionService } from 'src/app/service/transaction/transaction.service';
+import { DEFAULT_DATE_RANGE, FIELD_CATEGORY } from 'src/app/shared/transaction-constants';
 
 @Component({
   selector: 'app-tag-home',
   templateUrl: './tag-home.component.html',
   styleUrls: ['./tag-home.component.css']
 })
-export class TagHomeComponent {
+export class TagHomeComponent implements OnInit {
+
+  categoryType = FIELD_CATEGORY;
+
+  transactions: Transaction[];
 
   dateRange = DEFAULT_DATE_RANGE;
 
+  categorySelected: string;
+
+  constructor(private transactionService: TransactionService) {}
+
+  ngOnInit(): void {
+    this.loadTransactions();
+  }
+
+  loadTransactions() {
+    this.transactionService.search(this.dateRange.start, this.dateRange.end).subscribe((data: Transaction[]) => {
+      this.transactions = data;
+    });
+  }
+
   onDateChanged($event) {
     this.dateRange = $event;
+    this.loadTransactions();
+  }
+
+  onCategoryChanged($event) {
+    this.categorySelected = $event;
   }
 
 }
